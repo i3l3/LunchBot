ac75720a7e9a31503b2e299039efc7f4fbedee18
i3l3 <gdhan22@hints.or.kr>
2024-11-13 20:05:57 +0900

    feat: file download logging

--- init.py
+++ init.py
@@ -18,10 +18,13 @@ def init():
         os.mkdir("assets")
     if not path.exists("assets/background.png") and background_url != "":
         download(background_url, "assets/background.png")
+        print(f"Downloading background.png in {os.getcwd()}/assets/background.png")
     if not path.exists("assets/Pretendard-Bold.otf") and bold_font_url != "":
-        download(bold_font_url, "assets/Pretendard-Bold.otf.png")
+        download(bold_font_url, "assets/Pretendard-Bold.otf")
+        print(f"Downloading background.png in {os.getcwd()}/assets/Pretendard-Bold.otf")
     if not path.exists("assets/Pretendard-ExtraBold.otf") and extra_bold_font_url != "":
-        download(extra_bold_font_url, "assets/Pretendard-ExtraBold.otf.png")
+        download(extra_bold_font_url, "assets/Pretendard-ExtraBold.otf")
+        print(f"Downloading background.png in {os.getcwd()}/assets/Pretendard-ExtraBold.otf")
 
 if __name__ == "__main__":
     init()
