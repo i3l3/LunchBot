79a2f8b0dda9d8a5af9a63d47aac028d5fd12ef1
i3l3 <gdhan22@hints.or.kr>
2024-11-13 21:03:59 +0900

    feat: detect weekends

--- notice_daily.py
+++ notice_daily.py
@@ -2,10 +2,12 @@ import time
 import schedule
 import random
 import notice_manually
+from datetime import datetime
 
 def job():
-    time.sleep(random.randint(1, 5) * 60)
-    notice_manually.main()
+    if datetime.today().weekday() < 5:
+        time.sleep(random.randint(1, 5) * 60)
+        notice_manually.main()
 
 schedule.every().day.at("07:00", "Asia/Seoul").do(job())
 
