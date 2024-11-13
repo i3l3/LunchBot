67953fb260ba598533cda55a44a913cdc20a76c5
i3l3 <gdhan22@hints.or.kr>
2024-11-13 21:11:00 +0900

    fix: scheduler

--- notice_daily.py
+++ notice_daily.py
@@ -9,7 +9,11 @@ def job():
         time.sleep(random.randint(1, 5) * 60)
         notice_manually.main()
 
-schedule.every().day.at("07:00", "Asia/Seoul").do(job())
+def still_alive():
+    print(f"Still alive at {datetime.today()}")
+
+schedule.every(1).day.at("07:00", "Asia/Seoul").do(job)
+schedule.every(1).hours.do(still_alive)
 
 while True:
     schedule.run_pending()
